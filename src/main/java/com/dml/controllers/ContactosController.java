669e611069395cd69dc82c815ae3ba7b649cfbb8
Mejías López <diegomejiaslopez@gmail.com>
2020-03-16 19:36:52 +0100

    Tercer commit

---
+++ src/main/java/com/dml/controllers/ContactosController.java
@@ -0,0 +1,9 @@
+package com.dml.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ContactosController {
+
+	 @Autowired
+	 private ContactDAO contactDAO;
+}
